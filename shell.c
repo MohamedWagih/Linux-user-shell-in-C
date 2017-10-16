98045d5b4c6ad8b5f2a61f78646515ae47a05fb4
Mohamed Wagih <mohamed0wagih@gmail.com>
2017-10-16 17:20:17 +0200

    background and input

--- shell.c
+++ shell.c
@@ -3,81 +3,112 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <ctype.h>
-#include <signal.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <unistd.h>
 
+
+//global variables
 int in =0, out=0;
 char outputfile[20];
 char* argsout [1024];
-int main()
-{
+int length = 0;
+//------------
+
+char **takeInput(){
+
+    int index = 0;
+    char **command =(char**) calloc(1024,sizeof(char));
+
+    int k =0;
+    char buffer [1024];
+
+    int dquoted =0;
+    int squoted =0;
+
+    int c;
+    printf("sish:> ");
     while(1){
-        char *token;
-        char* tokens[1024] = { NULL };
-        char input[1024];
-        //int c = NULL;
-        int  i=0, j=0;
-        const char s[2] = " ";
-        printf("sish:>");
-        //take input
-        int c = getchar();
-        if (c == '\n'  || c =='\r'|| c =='\a'|| c =='\t' || c == ' '){
-            continue;
+
+        c = getchar();
+
+        if (c == '\n'){
+
+
+            if (buffer[0] == '\0' ||buffer[0] == '\t'){
+                strcpy(buffer,"\0");
+                length = index;
+                return command;
+
+            }
+            buffer[k] = '\0';
+            command[index] = (char*)malloc(sizeof(char) * strlen(buffer));
+            strcpy(command[index],buffer);
+            strcpy(buffer,"\0");
+            length = index+1;
+            return command;
+        }
+        else if (c == '"' && !squoted){
+            if(dquoted == 0)
+                dquoted = 1;
+            else
+                dquoted = 0;
+        }
+        else if (c == 39 &&! dquoted){
+            if(squoted == 0)
+                squoted = 1;
+            else
+                squoted = 0;
+        }
+        else if ((c ==' ' ||c == '\t')&& !squoted &&!dquoted){
+            if (buffer[0] == '\0'){
+                k = 0;
+                continue;
+            }
+            buffer[k] = '\0';
+
+            command[index] = (char*)malloc(sizeof(char) * strlen(buffer));
+            strcpy(command[index],buffer);
+            strcpy(buffer,"\0");
+            index++;
+            k = 0;
+        }
+        else if ((c =='<'||c =='>'||c =='|'||c =='&')&& !squoted &&!dquoted){
+                if(strlen(buffer) == 0){
+                buffer[k] = c;
+                k++;
+                }
+                else{
+                    buffer[k] = '\0';
+                    command[index] = (char*)malloc(sizeof(char) * strlen(buffer));
+                    strcpy(command[index],buffer);
+                    strcpy(buffer,"\0");
+                    index++;
+                    k = 0;
+                    buffer[k] = c;
+                    buffer[k+1] = '\0';
+                    command[index] = (char*)malloc(sizeof(char) * strlen(buffer));
+                    strcpy(command[index],buffer);
+                    strcpy(buffer,"\0");
+                    index++;
+                    k = 0;
+                }
+
         }
         else{
-            input[0] = c;
-                }
-        scanf("%[^\n]%*c", input+1);
-        if (!strcmp(input,"exit")){
-            return 0;
+            buffer[k] = c;
+            k++;
         }
-
-        /* get the first token */
-        token = strtok(input, s);
-        tokens[i] = token;
-        i++;
-
-        /* walk through other tokens */
-        while( token != NULL ){
-            token = strtok(NULL, s);
-            tokens[i] = token;
-            i++;
-        }
-
-        while(tokens[j]){
-          //printf("%s\n", tokens[j]);
-          if (!strcmp(tokens[j], ">")){
-            //printf("ouuuuuuut\n");
-            out=1;
-            strcpy(outputfile, tokens[j+1]);
-            break;
-            //printf("%s\n", outputfile);
-          }
-          argsout[j] = tokens[j];
-          j++;
-        }
-         //printf("%s\n", tokens[1]);
-
-        if(out){
-            printf("out to a file: \n");
-            prcs_start(argsout);
-        } else{
-            printf("out to screen: \n");
-            prcs_start(tokens);
-        }
-
     }
-    return(0);
+
+
 }
 
 
 
-
-int prcs_start(char **args){
-
+int prcs_start(char **args , int background){
   pid_t pid, wpid;
   int status;
 
@@ -90,29 +121,76 @@ int prcs_start(char **args){
         dup2(fd_output,STDOUT_FILENO);
         close (fd_output);
         out =0;
-
     }
     if (execvp(args[0], args) == -1) {
-      perror("ERROR");
-    }
-    //close (fd_output);
-
-
-
+          perror("ERROR");
+        }
     exit(EXIT_FAILURE);
   } else if (pid < 0) {
     // Error forking
     perror("ERROR");
   } else {
+    
     // Parent process
-    do {
-      wpid = waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    if(out){
-        out =0;
+    if (!background){
+      do {
+        wpid = waitpid(pid, &status, WUNTRACED);
+      } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        if(out){
+            out =0;
+        }
     }
-  }
-
+}
   return 1;
 }
 
+
+int main(){
+        int background = 0;
+        char **tokens;
+        while(1){
+            background = 0;
+            tokens = takeInput();
+            if (!tokens[0]){
+                continue;
+            }
+            if ((!tokens[1] &&  !strcmp(tokens[0],"exit") )){
+                return 0;
+            }
+            
+            //background check
+            if (length>1){
+              if (!strcmp(tokens[length -1],"&")){
+                background = 1;
+                tokens[length -1]= NULL;
+                }
+              }
+              
+              
+            int j = 0;
+            //file I/O check
+            while(tokens[j]){
+                  //printf("%s\n", tokens[j]);
+                  if (!strcmp(tokens[j], ">")){
+                    //printf("ouuuuuuut\n");
+                    out=1;
+                    strcpy(outputfile, tokens[j+1]);
+                    break;
+                    //printf("%s\n", outputfile);
+                  }
+                  argsout[j] = tokens[j];
+                  j++;
+            }  
+              
+            //run
+        if(out){
+            printf("out to a file: \n");
+            prcs_start(argsout,background);
+        } else{
+            printf("out to screen: \n");
+            prcs_start(tokens,background);
+        }
+            
+        }
+return 0;
+}
